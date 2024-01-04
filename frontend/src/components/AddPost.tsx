084edaed148931e1580f453906d1ca398c77e7a1
Dylan Almond <dylan@dylanalmond.net>
2024-01-04 20:03:00 +0000

    Increased max width of post form in frontend + updated backend .gitignore

--- frontend/src/components/AddPost.tsx
+++ frontend/src/components/AddPost.tsx
@@ -17,6 +17,8 @@ export const AddPost = () => {
     try {
       const response = await axios.post(`${apiURL}/posts`, data, { withCredentials: true });
 
+      console.log(response);
+
       setTimeout(() => {
         navigate(0);
       }, 1000);
@@ -26,7 +28,7 @@ export const AddPost = () => {
   };
 
   return (
-    <div className="w-full max-w-md mx-auto my-2">
+    <div className="w-full mx-auto my-2">
       <form onSubmit={(e) => { createPost(); e.preventDefault() }}>
         <div className="flex flex-col px-4 py-3.5 gap-1 mb-4 relative appearance-none border rounded-lg w-full text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
           <label htmlFor="content" className="w-0 h-0">
